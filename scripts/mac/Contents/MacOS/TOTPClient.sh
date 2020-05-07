bd9035d61f9930a67247fed5396545bfd842735b
repy <gamecube02+git@gmail.com>
2020-05-07 23:13:39 +0900

    ディレクトリ変更

--- scripts/mac/Contents/MacOS/TOTPClient.sh
+++ scripts/mac/Contents/MacOS/TOTPClient.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p "${HOME}/JDK"
-cd "${HOME}/JDK"
+mkdir -p "${HOME}/.totp"
+cd "${HOME}/.totp"
 "$(dirname $0)/image/bin/java" "-Xdock:icon=$(dirname $0)/../Resources/icon.png" -m "info.repy.totp/info.repy.totp.TOTPClient"
